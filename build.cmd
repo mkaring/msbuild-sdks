42c04beb6d77f8f077e32189ae697263a163f2f8
Nirmal Guru <Nirmal4G@gmail.com>
2017-10-30 11:00:22 +0530

    Some Improvements
    Trying out new methods

--- build.cmd
+++ build.cmd
@@ -6,20 +6,28 @@ set WD=%~dp0
 REM Check MyGet
 if not "%BuildRunner%" == "MyGet" (
 	set PackageVersion=%1
-	set nuget=nuget.exe
+
+	REM Enable VS DevEnv
+	call vsdevcmd -no_logo -arch=amd64 -host_arch=amd64 -app_platform=UWP
+	set MSBuildSdksPath=%Projects%\Packages\Sdks
 )
 
-REM Set Version
+REM Set Vars
+:: set Project=MSBuild.NET.Sdk.sln
+set Project=%WD%MSBuild.NET.Extras.Sdk\MSBuild.NET.Extras.Sdk.nuproj
+
+set Configuration=Release
+
 set version=
 if not "%PackageVersion%" == "" (
-	set version=-Version %PackageVersion%
+	set version=;PackageVersion=%PackageVersion%
 )
 
-REM Generate Package
-call %nuget% pack %WD%MSBuild.NET.Extras.Sdk\MSBuild.NET.Extras.Sdk.nuspec -OutputDirectory %WD%Packages %version%
+REM Build
+call msbuild %Project% /t:Restore;Build /p:Configuration=%Configuration%%version%
 
 REM Check MyGet
 if not "%BuildRunner%" == "MyGet" (
 	REM Push Package
-	call %nuget% push %WD%Packages\*.nupkg
+	call nuget push %WD%Packages\%Configuration%\*.nupkg
 )
